42049a0eeee1ecb9b55345258b35ebdad40b8ca8
Frank Hamand <frank@posthog.com>
2026-02-10 16:49:12 +0000

    again

--- posthog/clickhouse/migrations/0212_logs.py
+++ posthog/clickhouse/migrations/0212_logs.py
@@ -2,10 +2,11 @@ from django.conf import settings
 
 from posthog.clickhouse.client.connection import NodeRole
 from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
+from posthog.clickhouse.logs import TABLE_NAME
 
 operations = [
     run_sql_with_exceptions(
-        f"create TABLE if not exists logs AS logs32 ENGINE = Distributed('posthog', {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}, 'logs32');",
+        f"create TABLE if not exists {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}.logs AS {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}.{TABLE_NAME} ENGINE = Distributed('posthog', {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}, '{TABLE_NAME}');",
         node_roles=[NodeRole.LOGS],
     ),
 ]
