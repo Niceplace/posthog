8d83f2bb17adff0a1525f809081812e4019a5efc
Frank Hamand <frank@posthog.com>
2026-02-10 16:36:44 +0000

    .

--- posthog/clickhouse/migrations/0212_logs.py
+++ posthog/clickhouse/migrations/0212_logs.py
@@ -5,7 +5,7 @@ from posthog.clickhouse.client.migration_tools import run_sql_with_exceptions
 
 operations = [
     run_sql_with_exceptions(
-        f"create or replace TABLE logs AS logs32 ENGINE = Distributed('default', {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}, 'logs32');",
+        f"create or replace TABLE logs AS logs32 ENGINE = Distributed('posthog', {settings.CLICKHOUSE_LOGS_CLUSTER_DATABASE}, 'logs32');",
         node_roles=[NodeRole.LOGS],
     ),
 ]
